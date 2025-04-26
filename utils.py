06d7d401748dd4d666523b46fdd7a715c40169a8
Mu <mulixuan1994@gmail.com>
2025-04-27 08:55:07 +0900

    Update codes

--- utils.py
+++ utils.py
@@ -4,8 +4,8 @@ import torch
 
 # Convert CSV data to Tensor.
 # The shape of output data tensor is (N, H, W).
-def csv_to_tensor(dataframe: pd.DataFrame, is_train: bool = True) -> list:
-    if is_train is True:
+def csv_to_tensor(dataframe: pd.DataFrame, is_training: bool = True) -> list:
+    if is_training is True:
         data = torch.tensor(dataframe.iloc[:, 1:].to_numpy(), dtype=torch.float)
         data = data.reshape(data.size(0), 28, 28)
         label = torch.tensor(dataframe.iloc[:, 1].to_numpy(), dtype=torch.float)
@@ -21,13 +21,13 @@ def csv_to_tensor(dataframe: pd.DataFrame, is_train: bool = True) -> list:
 # Use min-max normalization to shrink pixel value to [0, 1].
 # The size of inputs and outputs tensors are both (N, H, W).
 # Note: Be sure dtype of inputs should be float.
-def normalize(inputs: torch.Tensor, norm: str = None) -> torch.Tensor:
-    if norm == "global":
+def normalize(inputs: torch.Tensor, normalization: str = None) -> torch.Tensor:
+    if normalization == "global":
         glo_max = torch.max(inputs).item()
         glo_min = torch.min(inputs).item()
         inputs = (inputs - glo_min) / (glo_max - glo_min)
 
-    elif norm == "per-image":
+    elif normalization == "per-image":
         inputs = torch.flatten(inputs, start_dim=1)
         img_max = torch.max(inputs, dim=1)[0].unsqueeze(1)
         img_min = torch.min(inputs, dim=1)[0].unsqueeze(1)
